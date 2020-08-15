e4999c99b0f7b742659354066a2fd0fc8c190175
Yi-Pei Chen <52116103+bbchen33@users.noreply.github.com>
2020-08-15 16:02:36 -0700

    np_function

--- new_flow.py
+++ new_flow.py
@@ -1,13 +1,9 @@
 
 import pandas as pd
-
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-
 # printing the current directory
 print(os.getcwd())
-
-print(np.random.randin(0,10e))
-
-print(os.getcwd())
+def np_function():
+    print(np.random.randint(10))
